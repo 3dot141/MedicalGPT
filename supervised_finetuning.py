40c8b89d19f40c09013654bdd0ab9e6dedeed9bb
Harrison <Harrison@fanruan.com>
2023-07-24 15:45:09 +0800

    add chatglm2 for finetune

--- supervised_finetuning.py
+++ supervised_finetuning.py
@@ -43,11 +43,18 @@ from transformers import (
     BitsAndBytesConfig,
     deepspeed,
 )
+from transformers.utils import (
+    ADAPTER_SAFE_WEIGHTS_NAME,
+    ADAPTER_WEIGHTS_NAME,
+    is_peft_available,
+    is_sagemaker_mp_enabled,
+)
 from transformers.trainer import TRAINING_ARGS_NAME
 
 MODEL_CLASSES = {
     "bloom": (BloomForCausalLM, BloomTokenizerFast),
     "chatglm": (AutoModel, AutoTokenizer),
+    "chatglm2": (AutoModel, AutoTokenizer),
     "llama": (LlamaForCausalLM, LlamaTokenizer),
     "baichuan": (AutoModelForCausalLM, AutoTokenizer),
     "auto": (AutoModelForCausalLM, AutoTokenizer),
@@ -204,6 +211,64 @@ class SavePeftModelTrainer(Trainer):
         self.model.save_pretrained(output_dir)
 
 
+class PeftTrainer(Trainer):
+    """
+    Trainer for lora models
+    """
+
+    def save_model(self, output_dir=None, _internal_call=False):
+        """Save the LoRA model."""
+        os.makedirs(output_dir, exist_ok=True)
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+        self.model.save_pretrained(output_dir)
+
+    def _load_from_peft_checkpoint(self, resume_from_checkpoint, model):
+        adapter_weights_file = os.path.join(resume_from_checkpoint, ADAPTER_WEIGHTS_NAME)
+        adapter_safe_weights_file = os.path.join(resume_from_checkpoint, ADAPTER_SAFE_WEIGHTS_NAME)
+
+        logger.info(f"resume_from_checkpoint is {resume_from_checkpoint}")
+
+        if not any(
+                os.path.isfile(f) for f in [adapter_weights_file, adapter_safe_weights_file]
+        ):
+            raise ValueError(f"Can't find a valid checkpoint at {resume_from_checkpoint}")
+
+        logger.info(f"Loading model from {resume_from_checkpoint}.")
+        # Load adapters following PR # 24096
+        if is_peft_available() and isinstance(model, PeftModel):
+            # If train a model using PEFT & LoRA, assume that adapter have been saved properly.
+            if hasattr(model, "active_adapter") and hasattr(model, "load_adapter"):
+                if os.path.exists(resume_from_checkpoint) or os.path.exists(resume_from_checkpoint):
+                    model.load_adapter(resume_from_checkpoint, model.active_adapter)
+                    # Load_adapter has no return value present, modify it when appropriate.
+                    from torch.nn.modules.module import _IncompatibleKeys
+
+                    load_result = _IncompatibleKeys([], [])
+                else:
+                    logger.warning(
+                        "The intermediate checkpoints of PEFT may not be saved correctly, "
+                        f"using `TrainerCallback` to save {ADAPTER_WEIGHTS_NAME} in corresponding folders, "
+                        "here are some examples https://github.com/huggingface/peft/issues/96"
+                    )
+            else:
+                logger.warning("Could not load adapter model, make sure to have `peft>=0.3.0` installed")
+
+    def _load_from_checkpoint(self, resume_from_checkpoint, model=None):
+
+        if model is None:
+            model = self.model_wrapped if is_sagemaker_mp_enabled() else self.model
+
+        if is_peft_available() and isinstance(model, PeftModel):
+            # Try to load adapters before trying to load a torch model
+            try:
+                return self._load_from_peft_checkpoint(resume_from_checkpoint, model=model)
+            except:
+                return super()._load_from_checkpoint(resume_from_checkpoint, model=model)
+            # If it is not a PeftModel, use the original _load_from_checkpoint
+        else:
+            return super()._load_from_checkpoint(resume_from_checkpoint, model=model)
+
+
 def save_model(output_dir, model, tokenizer, args):
     """Save the model and the tokenizer."""
     os.makedirs(output_dir, exist_ok=True)
@@ -313,7 +378,7 @@ def main():
         tokenizer_name_or_path = model_args.model_name_or_path
     tokenizer = tokenizer_class.from_pretrained(tokenizer_name_or_path, **tokenizer_kwargs)
     tokenizer.padding_side = "left"  # Set padding side equal to the collator's padding side
-    if model_args.model_type != "chatglm":
+    if model_args.model_type not in ["chatglm", "chatglm2"]:
         tokenizer.pad_token_id = 0  # Set pad token id to 0
     # Set special tokens for chatglm2
     if tokenizer.eos_token is None:
@@ -512,7 +577,7 @@ def main():
         padding="max_length",
         max_length=full_max_length,
     )
-    trainer = SavePeftModelTrainer(
+    trainer = PeftTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
